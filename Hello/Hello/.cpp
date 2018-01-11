d2a9005e981eda4423c71e70cda65e98f251ce6f
Ryan Owens <ryanvader82@gmail.com>
2018-01-10 20:41:23 -0600

    Started working on Block World

---
+++ Hello/Hello/.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+
+int main()
+{
+
+	cout << "Hello World!\n";
+	system("pause");
+	return 0;
+}
+
---
+++ Hello/Hello/helloWorld.cpp
@@ -0,0 +1,23 @@
+// sample main
+// your name 
+// project description
+
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <ctype.h>
+#include <string.h>
+#include <string>
+#include <stdlib.h>
+//#include <time.h>
+#include <ctime>
+
+using namespace std;
+
+int main ()
+{
+	cout << "Hello World!\n";
+	system("pause");
+	return 0;
+} // end of main
+
